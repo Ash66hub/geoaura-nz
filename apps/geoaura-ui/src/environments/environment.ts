df6c1c7614247c43eea6cfb36715fa4dc0a3de06
Aswanth <aswanth2000hb@gmail.com>
2026-05-03 18:55:39 +1200

    Add env file for ng

---
+++ apps/geoaura-ui/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/api/v1',
+  supabaseUrl: 'https://qzoievmtpylfdvbteruc.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6b2lldm10cHlsZmR2YnRlcnVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNDQ5MTIsImV4cCI6MjA5MjcyMDkxMn0.c3oXB_jxGlPw4IdlGwrHLXpdqB91CrrY3rvG2UESLkE'
+};
